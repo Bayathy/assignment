7f3e4302ca77db95af9f915432cb4870b9a987bb
Bayathy <misosoup0321@icloud.com>
2024-02-18 18:12:26 +0900

    chore: 🔨 kuma-uiの導入

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 /// <reference types="vitest" />
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import KumaUI from '@kuma-ui/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), KumaUI()],
   test: {
     name: 'react',
     root: './src',
