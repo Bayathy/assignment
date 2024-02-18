830e17634c49783dbe8b2a36126e1038866b800b
Bayathy <misosoup0321@icloud.com>
2024-02-19 00:10:50 +0900

    feat: 🚀 SWRConfigを用いてエラーハンドリングができるように
    後でエラー用ダイアログ等を表示するのに使う
    
    ✅ Closes: #5

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { SWRConfig } from 'swr'
 import App from './App.tsx'
 import '@unocss/reset/tailwind.css'
 
 import { setupMockWorker } from './mocks/browser.ts'
 
-setupMockWorker().then(() => {
-  ReactDOM.createRoot(document.getElementById('root')!).render(
-    <React.StrictMode>
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <SWRConfig value={{
+      onError: (err) => {
+        if (err.status !== 404)
+          console.error(err)
+      },
+    }}
+    >
       <App />
-    </React.StrictMode>,
-  )
-},
+    </SWRConfig>
+  </React.StrictMode>,
 )
