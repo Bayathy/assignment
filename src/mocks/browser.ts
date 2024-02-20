14317098221b8246dbeb031287ec5aa57ea7b3f4
Bayathy <misosoup0321@icloud.com>
2024-02-20 20:30:59 +0900

    fix: 🐞 serverの追加,mswを動くように修正

---
+++ src/mocks/browser.ts
@@ -0,0 +1,14 @@
+import { setupWorker } from 'msw'
+import { handlers } from './handler'
+
+export async function setupMockWorker() {
+  if (import.meta.env.DEV && import.meta.env.VITE_USE_MOCK_API === 'true') {
+    const worker = setupWorker(
+      ...handlers,
+    )
+
+    worker.start({
+      onUnhandledRequest: 'bypass',
+    })
+  }
+}
--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,9 @@ import KumaUI from '@kuma-ui/vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), KumaUI()],
+  server: {
+    cors: true,
+  },
   test: {
     name: 'react',
     root: './src',
