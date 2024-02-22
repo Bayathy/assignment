5db4ca7ad36fb803cbf59ea631f1fa76aee87b74
Bayathy <misosoup0321@icloud.com>
2024-02-22 11:55:09 +0900

    fix: 🐞 Chartコンポーネントの表示の修正

--- src/components/core/Chart/index.tsx
+++ src/components/core/Chart/index.tsx
@@ -13,11 +13,11 @@ interface Props {
 
 export const Chart: FC<Props> = ({ populations }) => {
   return (
-    <ResponsiveContainer height={400} width="90%">
+    <ResponsiveContainer height={400} width="100%">
       <LineChart width={600} height={300}>
         <CartesianGrid strokeDasharray="5 5" />
         <XAxis dataKey="year" tickFormatter={val => `${val}年`} allowDuplicatedCategory={false} />
-        <YAxis tickFormatter={val => `${val}万人`} />
+        <YAxis tickFormatter={val => `${val / 10000}万`} />
         <Tooltip />
         {populations.map(population => (
           <Line dataKey="value" data={population.data} name={population.label} key={population.label} />
