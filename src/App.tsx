1132d13cac95ef42f4b8f24cab7c9e75eb6ef41f
Bayathy <misosoup0321@icloud.com>
2024-02-22 11:55:31 +0900

    feat: 🚀 表示するUIレイアウトの更新

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,42 @@
+import { css } from '@kuma-ui/core'
 import { Chart } from './components/core/Chart'
+import { SelectPrefectureForm } from './components/core/SelectPrefectureForm'
+import { Header } from './components/ui/Header'
 import { population } from './mocks/data/population'
 
 function App() {
   return (
-    <div>
-      <Chart populations={[
-        {
-          label: '東京都',
-          data: population[0].result.data[0].data,
-        },
-        {
-          label: '大阪府',
-          data: population[1].result.data[0].data,
-        },
-      ]}
-      />
+    <div className={css`
+      min-height: 100vh;
+      display: grid;
+      grid-template-rows: auto 1fr;
+      grid-template-columns: 100%;
+      gap: 16px;
+    `}
+    >
+      <Header />
+      <main className={css`
+        max-width:1200px;
+        width:100%;
+        margin-inline:auto;
+        display: flex;
+        flex-direction: column;
+        gap: 16px;
+      `}
+      >
+        <SelectPrefectureForm />
+        <Chart populations={[
+          {
+            label: '東京都',
+            data: population[0].result.data[0].data,
+          },
+          {
+            label: '大阪府',
+            data: population[1].result.data[0].data,
+          },
+        ]}
+        />
+      </main>
     </div>
   )
 }
