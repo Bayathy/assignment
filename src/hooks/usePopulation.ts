4394b7622d0cea6ef0ffd7644c62d4ff93f2d892
Bayathy <misosoup0321@icloud.com>
2024-02-24 16:08:36 +0900

    fix: 🐞 custom hooksが要件を満たしてなかったので修正

--- src/hooks/usePopulation.ts
+++ src/hooks/usePopulation.ts
@@ -1,20 +1,47 @@
 import useSWR from 'swr'
-import { fetcher } from '../lib/fetcher'
-import type { Population } from '../model/population'
 
+import { useCallback } from 'react'
+import type { Population, PopulationResponse } from '../model/population'
+
+import { fetcher } from '../lib/fetcher'
+import type { Prefecture } from '../model/prefecture'
+
+async function requestHandler(prefectureList: Prefecture[]) {
+  const requests = prefectureList.map(prefecture => fetcher(`${import.meta.env.VITE_API_URL}/population/composition/perYear?prefCode=${prefecture.prefCode}`)
+    .then((res: PopulationResponse) => {
+      return {
+        prefName: prefecture.prefName,
+        data: res.result.data,
+      } as Population
+    }))
+
+  return Promise.all(requests)
+}
 /**
  * prefCodeに対応する都道府県の人口を取得する
  * @param prefCode
  * @returns
  */
 
-export function usePopulation(prefCode: number) {
-  const { data, isLoading, error } = useSWR<Population>(`${import.meta.env.VITE_API_URL}/population/composition/perYear?cityCode=-&prefCode=${prefCode}`, fetcher)
-  const population = data?.result
+export function usePopulations() {
+  const { data, isLoading, error, mutate } = useSWR<Population[]>(`${import.meta.env.VITE_API_URL}/population/composition/perYear`)
+  const fetchPopulation = useCallback((prefectureList: Prefecture[]) => {
+    mutate(requestHandler(prefectureList))
+  }, [mutate])
+
+  if (data === undefined) {
+    return {
+      populations: [],
+      isLoading,
+      fetchPopulation,
+      error,
+    }
+  }
 
   return {
-    population,
+    populations: data,
     isLoading,
     error,
+    fetchPopulation,
   }
 }
--- src/hooks/usePrefectures.ts
+++ src/hooks/usePrefectures.ts
@@ -1,5 +1,5 @@
 import useSWR from 'swr'
-import type { Prefecture } from '../model/prefecture'
+import type { PrefectureResponse } from '../model/prefecture'
 import { fetcher } from '../lib/fetcher'
 
 /**
@@ -8,7 +8,15 @@ import { fetcher } from '../lib/fetcher'
  */
 
 export function usePrefectures() {
-  const { data, isLoading, error } = useSWR<Prefecture>(`${import.meta.env.VITE_API_URL}/prefectures`, fetcher)
+  const { data, isLoading, error } = useSWR<PrefectureResponse>(`${import.meta.env.VITE_API_URL}/prefectures`, fetcher)
+
+  if (data?.result === undefined) {
+    return {
+      prefectures: [],
+      isLoading,
+      error,
+    }
+  }
 
   const prefectures = data?.result
 
