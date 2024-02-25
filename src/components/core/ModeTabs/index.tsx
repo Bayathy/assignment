5b6dd29a1a71b07201dde84e39d7e0f8ef56071c
Bayathy <misosoup0321@icloud.com>
2024-02-26 04:59:59 +0900

    feat: 🚀 TabTriggerの追加

--- src/components/core/ModeTabs/index.tsx
+++ src/components/core/ModeTabs/index.tsx
@@ -1,6 +1,8 @@
 import * as Tabs from '@radix-ui/react-tabs'
 import type { FC } from 'react'
 import React from 'react'
+import { css } from '@kuma-ui/core'
+import { TabsTrigger } from './Trigger'
 
 interface Props {
   totalGraph: React.ReactNode
@@ -18,11 +20,20 @@ export const ModeTabs: FC<Props> = ({
   return (
     <form>
       <Tabs.Root defaultValue="total">
-        <Tabs.List>
-          <Tabs.Trigger value="total">全体</Tabs.Trigger>
-          <Tabs.Trigger value="juniors">年少人口</Tabs.Trigger>
-          <Tabs.Trigger value="working">生産年齢人口</Tabs.Trigger>
-          <Tabs.Trigger value="old">老年人口</Tabs.Trigger>
+        <Tabs.List className={css`
+          display: grid;
+          grid-template-columns: repeat(4, 1fr);
+          gap: 16px;
+          padding: 8px;
+          border-radius: 8px;
+          background-color: t("colors.primary");
+          margin-bottom: 16px;
+        `}
+        >
+          <TabsTrigger value="total">全体</TabsTrigger>
+          <TabsTrigger value="juniors">年少人口</TabsTrigger>
+          <TabsTrigger value="working">生産年齢人口</TabsTrigger>
+          <TabsTrigger value="old">老年人口</TabsTrigger>
         </Tabs.List>
         <Tabs.Content value="total">{totalGraph}</Tabs.Content>
         <Tabs.Content value="juniors">{juniorsGraph}</Tabs.Content>
