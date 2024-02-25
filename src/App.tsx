afb6aa9f3fe30afbda40637c3538b568cb4b95f7
Bayathy <misosoup0321@icloud.com>
2024-02-26 04:01:30 +0900

    refactor: 🔧 Toggleの押下状態をキープできるように

--- src/App.tsx
+++ src/App.tsx
@@ -7,48 +7,70 @@ import { ModeTabs } from './components/core/ModeTabs'
 import { usePrefectures } from './hooks/usePrefectures'
 import type { Prefecture } from './model/prefecture'
 import { usePopulations } from './hooks/usePopulations'
+import { useMediaQuery } from './hooks/useMediaQuery'
+import { Dialog } from './components/ui/Dialog'
 
 function App() {
+  const isMobile = useMediaQuery('(max-width: 768px)')
+
   const { prefectures } = usePrefectures()
-  const [selectedPrefectures, setSelectedPrefectures] = useState<Prefecture[]>([])
+  const [selectedPrefectures, setSelectedPrefectures] = useState<Prefecture[]>(
+    [],
+  )
   const { populations, isLoading } = usePopulations(selectedPrefectures)
 
-  const toggleSelectPrefecture = (pressed: boolean) =>
-    (selectPrefecture: Prefecture) => {
-      const newList = pressed ? [selectPrefecture, ...selectedPrefectures] : selectedPrefectures.filter(prefecture => prefecture.prefCode !== selectPrefecture.prefCode)
+  const toggleSelectPrefecture
+    = (pressed: boolean) => (selectPrefecture: Prefecture) => {
+      const newList = pressed
+        ? [selectPrefecture, ...selectedPrefectures]
+        : selectedPrefectures.filter(
+          prefecture => prefecture.prefCode !== selectPrefecture.prefCode,
+        )
 
       setSelectedPrefectures(newList)
     }
 
   return (
-    <div className={css`
-      min-height: 100vh;
-      display: grid;
-      grid-template-rows: auto 1fr;
-      grid-template-columns: 100%;
-      gap: 16px;
-    `}
+    <div
+      className={css`
+        min-height: 100vh;
+        display: grid;
+        grid-template-rows: auto 1fr;
+        grid-template-columns: 100%;
+        gap: 16px;
+      `}
     >
       <Header />
-      <main className={css`
-        max-width:1200px;
-        width:100%;
-        margin-inline:auto;
-        display: flex;
-        flex-direction: column;
-        gap: 16px;
-        padding-inline: 16px;
-      `}
+      <main
+        className={css`
+          max-width: 1200px;
+          width: 100%;
+          margin-inline: auto;
+          display: flex;
+          flex-direction: column;
+          gap: 16px;
+          padding-inline: 16px;
+        `}
       >
         {prefectures
-        && (
-          <SelectPrefectureForm
-            prefectures={prefectures}
-            handleSelectPrefecture={toggleSelectPrefecture}
-          />
-        )}
-        { populations && !isLoading
-        && (
+        && (isMobile
+          ? (
+            <Dialog triggerText="都道府県の選択">
+              <SelectPrefectureForm
+                prefectures={prefectures}
+                selectPrefecture={selectedPrefectures}
+                handleSelectPrefecture={toggleSelectPrefecture}
+              />
+            </Dialog>
+            )
+          : (
+            <SelectPrefectureForm
+              prefectures={prefectures}
+              selectPrefecture={selectedPrefectures}
+              handleSelectPrefecture={toggleSelectPrefecture}
+            />
+            ))}
+        {populations && !isLoading && (
           <ModeTabs
             totalGraph={<Chart mode="total" populationList={populations} />}
             juniorsGraph={<Chart mode="juniors" populationList={populations} />}
