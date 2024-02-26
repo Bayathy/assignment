e8dd2793689606007b4b9f451ba329476b11ab92
Bayathy <misosoup0321@icloud.com>
2024-02-26 14:03:16 +0900

    docs: 📄 jsdocの整備

--- src/model/population.ts
+++ src/model/population.ts
@@ -1,14 +1,13 @@
+/**
+ * 人口データの種類を分けるための型
+ */
 export type PopulationTyoes = 'total' | 'working' | 'juniors' | 'old'
 
+/**
+ * 人口データの型、データの種類別にデータを持つ
+ */
 export interface Population {
   prefName: string
-  // data: {
-  //   mode: string
-  //   data: {
-  //     year: number
-  //     value: number
-  //   }[]
-  // }[]
   data: {
     [key in PopulationTyoes]: {
       year: number
@@ -17,6 +16,9 @@ export interface Population {
   }
 }
 
+/**
+ * 人口データのAPIレスポンスの型
+ */
 export interface PopulationResponse {
   message: string
   result: {
--- src/model/prefecture.ts
+++ src/model/prefecture.ts
@@ -1,8 +1,15 @@
+/**
+ * Prefecture Responseを整形した後の型
+ */
+
 export interface Prefecture {
   prefCode: number
   prefName: string
 }
 
+/**
+ * 都道府県のAPIレスポンスの型
+ */
 export interface PrefectureResponse {
   message: string
   result: Prefecture[]
